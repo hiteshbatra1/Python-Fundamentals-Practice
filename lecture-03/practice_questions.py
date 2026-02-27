788d6d73a8dea9d515338a8f7e563d1798929530
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-02-27 22:22:34 +0530

    Add practice question

--- lecture-03/practice_questions.py
+++ lecture-03/practice_questions.py
@@ -7,3 +7,14 @@ if(user_input == user_input_reverse_copy):
     print(f"Yes {user_input} Is An Palindrome")
 else:
     print(f"No {user_input} Is Not An Palindrome. Please Try Again With Diffrent Word")
+
+
+# Q2 Given a list of integers compute the average of all numbers in the list.
+
+list_of_numbers = [10,20,30,40,50]
+sum = 0
+for val in list_of_numbers:
+    sum += val
+
+avg = sum/len(list_of_numbers)
+print(avg)  
