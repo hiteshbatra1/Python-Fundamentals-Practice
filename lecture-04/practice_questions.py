a99d1d5204e47291e2b3ea616d49a950a9834fd6
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-03-28 22:02:59 +0530

    Add practice solution

--- lecture-04/practice_questions.py
+++ lecture-04/practice_questions.py
@@ -33,3 +33,43 @@ acc1.withdraw(50000)
 acc1.check_balance()
 
 print(acc1.account_number,acc1.owner_name,acc1.balance)
+
+
+#Q2 Create a Class Book with attributes title, author, and year. Include a method to display book details.
+
+class Book:
+    reviews = []
+    def __init__ (self,title,author):
+        self.title = title
+        self.author = author
+
+    #Method To Add Review To A Book:
+    def add_reviews (self, review):
+         self.reviews.append(review)
+
+    #Method To Count All Reviews To A Book
+    def count_reviews (self):
+        review_array = self.reviews
+        review_count = len(review_array)
+        print(f"Total Reviews To {self.title} Book By {self.author} are {review_count}")
+
+    #Method To Display Reviews Of A Specific Book:
+    def display_reviews (self):
+        review_array = self.reviews
+        for review in review_array:
+            print(review)
+
+#Instance Of Book Class:       
+B1 = Book("Hello World", "Hitesh")
+
+# Adding Reviews To Specific Book:
+B1.add_reviews("Very Good")
+B1.add_reviews("Good")
+
+# To Get Total Reviews On A Book:
+B1.count_reviews()
+
+# To Get Each Review Of Specific Book:
+B1.display_reviews()
+
+print(B1.title,B1.author,B1.reviews)
