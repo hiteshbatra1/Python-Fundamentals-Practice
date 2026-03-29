ee7d2819b4104eff0d4d15be6c1df87347e828fb
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-03-29 21:17:30 +0530

    Add practice question

--- lecture-04/practice_questions.py
+++ lecture-04/practice_questions.py
@@ -73,3 +73,55 @@ B1.count_reviews()
 B1.display_reviews()
 
 print(B1.title,B1.author,B1.reviews)
+
+
+#Q3 Create a Student class with private attributes for name , roll number and marks. Provide getter and setter methods for each attribute with validation.
+
+#  Student Class With Private Attributes And Getter-Setter Methods
+class Student:
+    def __init__ (self, name , roll_no, marks):
+        self.__name = name
+        self.__roll_no = roll_no
+        self.__marks = marks
+
+# Getter Methods 
+    def get_name (self):
+        print(f"Name is {self.__name}")
+
+    def get_roll_no (self):
+        print(f"Roll No. is {self.__roll_no}")    
+
+    def get_marks (self):
+        print(f"Marks is {self.__marks}")  
+
+# Setter Methods
+    def set_name (self , name):
+        if(name == ""):
+            print("Name Cannot Be Empty")
+            return
+        self.__name = name   
+
+    def set_rollno (self, roll_no):
+        if (roll_no < 1 or roll_no > 100):
+            print("Please Provide Roll No. Between 1 To 100")
+            return
+        self.__roll_no = roll_no
+
+    def set_marks (self, marks):
+        if(marks < 0):
+            print("Please Provide Valid Marks")    
+            return
+        self.__marks = marks
+
+# Instance Of Student Class
+stu1 = Student("Hitesh", 1, 100 )
+
+# Using Setter Methods To Set Values With Validation
+stu1.set_name("Hitesh")
+stu1.set_rollno(100)
+stu1.set_marks(100)
+
+# Using Getter Methods To Get Values
+stu1.get_name()
+stu1.get_roll_no()
+stu1.get_marks()
