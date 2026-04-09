8afa95ca525a06c235845603f050c762b6df6d84
hiteshbatra1 <hiteshbatra967@gmail.com>
2026-04-09 22:17:46 +0530

    Add Practice Solution

---
+++ lecture-05/practice_questions.py
@@ -0,0 +1,15 @@
+#Q1 Create a program that: 
+# 1. Opens a file in write mode "names.txt", 
+# 2. Writes 5 names (one per line) entered by the user, 
+# 3. Then opens the same file in read mode and prints all names.
+
+with open("names.txt", "w") as f:
+    for i in range(6):
+        name = input("Enter Name: ")
+        f.write(name + "\n")
+
+data = True      
+with open("names.txt","r") as f:
+    while data:
+        data = f.readline()
+        print(data)
